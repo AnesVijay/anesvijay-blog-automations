7652a11b83fce9162b7362822999a42ea12a4eb3
AnesVijay <2011andreyka@gmail.com>
2026-03-21 15:54:03 +0500

    copy content v0.1

--- update_content.py
+++ update_content.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 def main():
-    pass
+    load_dotenv()
+    obsidian_path = os.getenv("OBSIDIAN_VAULT")
+    hugo_path = os.getenv("HUGO_CONTENT")
+
+    # run ansible passing those vars there via flags
 
 if __name__ == "__main__":
     main()
